a7336cabca5f76dad6b420ceaa902d6790d9d2fa
pashhha <pavils.kuzmins@gmail.com>
2025-06-08 22:12:23 +0300

    debbaging fixes

--- services/customer/src/main/java/com/pavils/ecommerce/customer/CustomerRepository.java
+++ services/customer/src/main/java/com/pavils/ecommerce/customer/CustomerRepository.java
@@ -1,6 +1,8 @@
 package com.pavils.ecommerce.customer;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 }
